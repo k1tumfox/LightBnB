05f8317c0d79f04204219c36305879fb3fb8b8d3
Tenyson Kwon <tenysonkwon@gmail.com>
2020-09-18 00:03:24 +0000

    completed reservation query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -89,7 +89,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+    select reservations.*, properties.*, avg(rating) as average_rating
+    from reservations
+    join properties on properties.id = property_id
+    join property_reviews on reservation_id = reservations.id
+    where reservations.guest_id = $1
+    and reservations.end_date < now()::date
+    group by properties.id, reservations.id
+    order by reservations.start_date
+    limit $2
+  `, [guest_id, limit])
+  .then(res => res.rows);
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
